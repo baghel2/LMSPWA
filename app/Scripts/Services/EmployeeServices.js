e5632ade1f588394c50adc95761329bacd049557
Arvind singh <arvindsinghbaghel700@gmail.com>
2019-02-14 10:45:45 +0530

    srv scrt

---
+++ app/Scripts/Services/EmployeeServices.js
@@ -0,0 +1,52 @@
+﻿LMSApp.service("EmployeeServices", function ($http) {
+    var url = "http://localhost:17351/api/Employee/";
+    var $this = this;
+    $this.Add = function (EmployeeMDL) {
+        var request = $http({
+            method: 'POST',
+            url: url+"Add",
+            data: JSON.stringify(EmployeeMDL),
+            dataType: "json"
+        });
+        return request;
+      
+    }
+
+    $this.Delete = function (id) {
+        var request = $http({
+            method: 'POST',
+            url: '/Home/DeleteLeave',
+            data: "{ id:" + id + " }",
+            dataType: "json"
+        });
+        return request;
+    }
+
+    $this.GetList = function () {
+        var request = $http({
+            method: 'GET',
+            url: url +'GetList'
+        });
+        return request;
+        debugger;
+    }
+    
+    $this.GetLeave = function (id) {
+        var Url = '/Home/GetLeave/'+id+'';
+        var request = $http({
+            method: 'GET',
+            url: Url         
+        });
+        return request;
+    }
+
+    $this.Login = function (id, password) {
+        var Url = '/Login/Index?id=' + id + '&paswword= ' + password + '';
+        var request = $http({
+            method: 'GET',
+            url: Url
+        });
+        return request;
+    }
+
+});
---
+++ app/Scripts/Services/LeaveServices.js
@@ -0,0 +1,54 @@
+﻿LMSApp.service("LeaveServices", function ($http) {
+    var apiurl = '';
+     apiurl = "http://localhost:17351/api/Leave/";
+    var $this = this;
+
+    $this.Add = function (user) {
+
+        var request = $http({
+            method: 'POST',
+            url: apiurl + "Add",
+            data: JSON.stringify(user),
+            dataType: "json"
+        });
+        return request;
+      
+    }
+
+    $this.Delete = function (id) {
+        var request = $http({
+            method: 'POST',
+            url: '/Home/DeleteLeave',
+            data: "{ id:" + id + " }",
+            dataType: "json"
+        });
+        return request;
+    }
+
+    $this.GetList = function (id) {
+        var request = $http({
+            method: 'GET',
+            url: apiurl + 'GetList?id=' + id
+        });
+        return request;
+    }
+    
+    $this.GetLeave = function (id) {
+        var Url = '/Home/GetLeave/'+id+'';
+        var request = $http({
+            method: 'GET',
+            url: Url         
+        });
+        return request;
+    }
+
+    $this.Login = function (id, password) {
+        var Url = '/Login/Index?id=' + id + '&paswword= ' + password + '';
+        var request = $http({
+            method: 'GET',
+            url: Url
+        });
+        return request;
+    }
+
+});
