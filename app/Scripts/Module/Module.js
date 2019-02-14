94e886da270f8edbe0fe77e8c35213404aa0e3da
Arvind singh <arvindsinghbaghel700@gmail.com>
2019-02-14 10:44:46 +0530

    mdl scrt

---
+++ app/Scripts/Module/Module.js
@@ -0,0 +1,24 @@
+﻿var LMSApp = angular.module("LMSApp", ["ngRoute"]);
+
+LMSApp.config(['$routeProvider', '$locationProvider',function ($routeProvider, $locationProvider) {
+
+    $routeProvider
+    //.when("/", {
+    //    templateUrl: "index.html"
+    //})
+    .when("/list", {
+        templateUrl: "app/LeaveList.html"
+    })
+    .when("/leavedetail", {
+        templateUrl: "app/LeaveDetail.html",
+        controller: "LeaveController"
+    })
+        .when("/employee", {
+            templateUrl: "app/Employee.html",
+            controller: "EmployeeController"
+        })
+    .otherwise({
+        templateUrl: "app/Home.html"
+    });
+    $locationProvider.html5Mode(false).hashPrefix('!');
+}]);
